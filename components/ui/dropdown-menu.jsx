8c49a94a5839b6cbc391dcdbf13f2bc547df0b16
Sidd <siddharth726@outlook.com>
2024-08-17 07:13:46 -0700

    [FIX] slight changes to dropdown menu

--- components/ui/dropdown-menu.jsx
+++ components/ui/dropdown-menu.jsx
@@ -59,7 +59,7 @@ const DropdownMenuContent = React.forwardRef(
         ref={ref}
         sideOffset={sideOffset}
         className={cn(
-          "z-50 min-w-[8rem] overflow-hidden border border-neutral-200 bg-white p-1 text-neutral-950 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-50",
+          "z-50 min-w-[8rem] overflow-hidden border border-neutral-200 bg-white text-neutral-950 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-50",
           className,
         )}
         {...props}
@@ -74,7 +74,7 @@ const DropdownMenuItem = React.forwardRef(
     <DropdownMenuPrimitive.Item
       ref={ref}
       className={cn(
-        "relative flex cursor-default select-none items-center px-2 py-1.5 text-sm outline-none focus:text-pink-700 focus:bg-pink-100/50 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-pink-500/50 dark:focus:text-pink-100",
+        "relative flex cursor-default select-none items-center px-2 py-1.5 text-sm  outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 text-neutral-900 dark:border-neutral-800 border-2 border-neutral-200 focus:bg-neutral-100 dark:text-neutral-50 dark:focus:bg-neutral-800",
         inset && "pl-8",
         className,
       )}
